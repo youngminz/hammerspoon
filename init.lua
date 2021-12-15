5073398020717436cd2657f045d152208fc5f462
Youngmin Koo <youngminz.kr@gmail.com>
2021-12-16 02:22:30 +0900

    Add capslock-f18 mapping

--- init.lua
+++ init.lua
@@ -1,3 +1,9 @@
 require('modules.terminal')
 require('modules.kakaotalk')
 --require('modules.inputsource_aurora')
+
+-- https://www.philgineer.com/2021/01/m1-hammerspoon.html
+local FRemap = require('modules.foundation_remapping')
+local remapper = FRemap.new()
+remapper:remap('capslock', 'f18')
+remapper:register()
